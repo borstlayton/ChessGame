c7aeb285124136852a90406134833c3a0c7e27ca
borst2 <borst2@stolaf.edu>
2024-08-02 15:14:01 -0500

    Init

---
+++ Scripts/Main.gd
@@ -0,0 +1,22 @@
+extends Control
+
+@onready var play_button = $PlayButton
+@onready var title = $Title
+@onready var _screen_size_x = get_viewport_rect().size.x
+@onready var _screen_size_y = get_viewport_rect().size.y
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#Center the position of the Title and PlayButton
+	title.position = Vector2((_screen_size_x-title.get_rect().size.x)/2,_screen_size_y/5)
+	play_button.position = Vector2((_screen_size_x-play_button.get_rect().size.x)/2,_screen_size_y/2)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_play_button_pressed():
+	var level_select = load()
+	pass # Replace with function body.
