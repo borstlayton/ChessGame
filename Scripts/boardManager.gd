bd2051fd2319d121ca1f37f256bda65abfcd274f
borst2 <borst2@stolaf.edu>
2024-08-08 12:51:28 -0500

    Pawn moves

--- Scripts/boardManager.gd
+++ Scripts/boardManager.gd
@@ -66,8 +66,66 @@ func get_valid_tiles(row, column):
 	elif piece == 'k' || piece == 'K':
 		get_king_move(row, column, piece)
 
+func _move(row, column, color):
+	if row >= 0 and row <= 7 and column >=0 and row <= 7:
+		if current_board[row][column] == 0:
+			return vector2(row, column)
+		else:
+			return 0
+	else:
+		return 0
+
+#Color is the color of the moving piece. 1 if white and 0 if black
+func _attack(row, column, color):
+	#If the square is in bounds
+	if row >= 0 and row <= 7 and column >=0 and row <= 7:
+		#Color of the piece on the target square. 1 if white and 0 if black
+		var target_color = (current_board[row][column] == current_board[row][column].to_upper())
+		#If the square isn't blank and the target color is the oposite of the color of the moving piece, return the square to be added to moves.
+		if current_board[row][column] != 0 and color != target_color:
+			return vector2(row, column)
+		else:
+			return 0
+
 func get_pawn_move(row, column, piece):
-	pass
+	var legal_moves = []
+	var idx = 0
+	var temp
+	#If a white piece
+	if piece == "P":
+		#Move Forward
+		temp = _move(row+1,column, true)
+		if temp != 0:
+			legal_moves[idx] = temp
+			idx += 1
+		#Attack right diagonally
+		temp = _attack(row+1,column+1, true)
+		if temp != 0:
+			legal_moves[idx] = temp
+			idx += 1
+		#Attack left diagonally
+		temp = _attack(row+1,column+1, true)
+		if temp != 0:
+			legal_moves[idx] = temp
+			idx += 1	
+	elif piece == "p":
+		#Move Forward
+		temp = _move(row-1,column, false)
+		if temp != 0:
+			legal_moves[idx] = temp
+			idx += 1
+		#Attack right diagonally
+		temp = _attack(row-1,column+1, false)
+		if temp != 0:
+			legal_moves[idx] = temp
+			idx += 1
+		#Attack left diagonally
+		temp = _attack(row+1,column+1, false)
+		if temp != 0:
+			legal_moves[idx] = temp
+			idx += 1	
+	return legal_moves
+	
 func get_bishop_move(row, column, piece):
 	pass
 func get_knight_move(row, column, piece):
