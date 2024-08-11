7c1638ac4f5e22b54489e6f257064c56e77f02ae
Jorsheer <88947796+Jorsheer@users.noreply.github.com>
2024-08-10 21:53:59 -0500

    comments

--- Scripts/GridContainer.gd
+++ Scripts/GridContainer.gd
@@ -3,6 +3,8 @@ extends GridContainer
 var original_color = Color("b52d00")
 
 func show_tiles(tiles_shown):
+	if tiles_shown == null:
+		return
 	for tile in tiles_shown:
 		var ID = 8*tile.x + tile.y
 		get_child(ID).set_color(Color.DARK_OLIVE_GREEN)
@@ -14,7 +16,7 @@ func clear_valid_tiles():
 				current_tile.set_color(Color.BEIGE)
 			else:
 				current_tile.set_color(original_color)
-func move_pieces(past_tile : Vector2, next_tile : Vector2):
+func move_pieces(past_tile : Vector2, next_tile : Vector2): #NOT IN USE YET
 	var past_ID = past_tile.x * 8 + past_tile.y
 	var next_tile_ID = next_tile.x*8 + next_tile.y
 	
