71d7f7219e94d8ffa175b075c57a964d30de16a2
Jorsheer <88947796+Jorsheer@users.noreply.github.com>
2024-08-15 10:53:28 -0500

    coin amounts

--- Scripts/CoinCounter.gd
+++ Scripts/CoinCounter.gd
@@ -1,7 +1,5 @@
 extends Label
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	pass
-#	text = "Coins: " + str(CoinManager.coin_amount)
+	text = str(ShopManager.current_coin_amount)
--- Scripts/ShopManager.gd
+++ Scripts/ShopManager.gd
@@ -1,14 +1,24 @@
 extends Node
 
+var current_coin_amount : int
 var current_state : piece_purchase_states
 var current_piece : String
 var current_piece_scene
 var purchased_pieces_locations = []
 var purchased_pieces_names = []
+
 enum piece_purchase_states{PURCHASE_IDLE, MOVING_PIECE, PIECE_PLACED}
 
+var coin_amount := {
+	"P": 1,
+	"B": 3,
+	"N": 3,
+	"R": 5,
+	"Q": 8,
+}
 func _ready():
 	SignalManager.placed_purchased_piece.connect(add_piece)
+	current_coin_amount = 100
 func purchase_piece(piece_type : String):
 	current_piece = piece_type
 	SignalManager.purchased_piece.emit(piece_type)
@@ -16,3 +26,7 @@ func purchase_piece(piece_type : String):
 func add_piece(location : Vector2, piece : String):
 	purchased_pieces_locations.append(location)
 	purchased_pieces_names.append(piece)
+	subtract_coin_amount(piece)
+	
+func subtract_coin_amount(piece : String):
+	current_coin_amount -= coin_amount[piece]
