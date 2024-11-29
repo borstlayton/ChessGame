855738005825dc5c844f548599dcf5b6c4c74ccd
Jorsheer <88947796+Jorsheer@users.noreply.github.com>
2024-11-29 11:46:47 -0600

    random cards

--- Scripts/CardDisplay.gd
+++ Scripts/CardDisplay.gd
@@ -13,16 +13,7 @@ func generate_shop():
 	for card_container in [first_card, second_card, third_card]:
 		card_container.show()
 		# Instantiate a new card
-		var new_card = CardManager.card_template.instantiate()
-
-		# Connect the Area2D signal dynamically
-		var area_2d = new_card.get_node("Area2D")
-		if area_2d:
-			print("good")
-			print(new_card.get_node("Area2D/CollisionShape2D"))
-		else:
-			print("Error: Area2D not found in the instantiated card.")
-
+		var new_card = CardManager.get_card()
 		# Add the card to the corresponding container
 		card_container.add_child(new_card)
 
--- Scripts/CardManager.gd
+++ Scripts/CardManager.gd
@@ -4,10 +4,12 @@ extends Node
 @onready var card_template = preload("res://Scenes/Cards/CardTemplate.tscn")
 var deck = []
 
-func ready():
-	var knight_scene_instance = knight_scene.instantiate()
-	deck.append(knight_scene_instance)
+func _ready():
+	var knight_scene_instance = knight_scene
+	deck.append(knight_scene)
 	
-	var card_template_instance = card_template.instantiate()
-	deck.append(card_template_instance)
-	print(deck)
+	var card_template_instance = card_template
+	deck.append(card_template)
+
+func get_card():
+	return deck.pick_random().instantiate()
