6e057b25d6b7aba14e9d97a6c78e579d6e0bf607
Jorsheer <88947796+Jorsheer@users.noreply.github.com>
2025-01-14 20:23:19 -0600

    string for piece taken functionality

--- Scripts/Board/last_piece_taken.gd
+++ Scripts/Board/last_piece_taken.gd
@@ -1,13 +1,20 @@
 extends Control
 
 @onready var sprite = $PieceTaken
-
+@onready var piece_price_label = $PiecePriceLabel
 func _ready():
 	SignalManager.captured_piece.connect(determine_piece)
 	
 func determine_piece(piece_taken : String, _piece_used : String, _column : int, _row : int, _past_column : int, _past_row : int):
 	if piece_taken == piece_taken.to_lower(): #is black piece
 		sprite.texture = load(BoardManager.assets[BoardManager.fen_dict[piece_taken]])
-
+		update_text(piece_taken)
+		
 func update_sprite(updated_sprite : Texture2D):
 	sprite.texture = updated_sprite
+
+func update_text(piece_taken : String):
+	var piece_value = RoundManager.piece_values[piece_taken]
+	var bounty_amount = BountyManager.retreive_bounty(piece_taken)
+	var price_text : String = str("Piece Value: ", piece_value, " X ", "Bounty: ", bounty_amount," X ", "Modifier Bonus: ")
+	piece_price_label.text = price_text
--- Scripts/Managers/BountyManager.gd
+++ Scripts/Managers/BountyManager.gd
@@ -12,3 +12,16 @@ func _ready():
 	rook_bounty_amount = 1
 	bishop_bounty_amount = 1
 	pawn_bounty_amount = 1
+
+func retreive_bounty(piece_name):
+	match piece_name:
+		"k":
+			return 1
+		"p":
+			return pawn_bounty_amount
+		"n":
+			return knight_bounty_amount
+		"q":
+			return queen_bounty_amount
+		"r":
+			return rook_bounty_amount
